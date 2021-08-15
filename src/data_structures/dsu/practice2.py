38edfa3b25f002ae872003d8efa3c58810c0fdff
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-08-15 14:21:30 +0900

    :sparkles: dsu

---
+++ src/data_structures/dsu/practice2.py
@@ -0,0 +1,58 @@
+
+class DSU:
+    def __init__(self, n: int = 0):
+        self._n = n
+        self.parent_or_size = [-1] * n
+
+    def merge(self, a: int, b: int):
+        x = self.leader(a)
+        y = self.leader(b)
+
+        if x == y:
+            return x
+
+        if -self.parent_or_size[x] < -self.parent_or_size[y]:
+            x, y = y, x
+
+        self.parent_or_size[x] += self.parent_or_size[y]
+        self.parent_or_size[y] = x
+        return x
+
+    def same(self, a: int, b: int):
+        return self.leader(a) == self.leader(b)
+
+    def leader(self, a: int):
+        parent = self.parent_or_size[a]
+        while parent >= 0:
+            if self.parent_or_size[parent] < 0:
+                return parent
+            self.parent_or_size[a], a, parent = (
+                self.parent_or_size[parent],
+                self.parent_or_size[parent],
+                self.parent_or_size[self.parent_or_size[parent]]
+            )
+
+        return a
+
+    def size(self, a: int):
+        return -self.parent_or_size[self.leader(a)]
+
+    def groups(self):
+        leader_buf = [self.leader(i) for i in range(self._n)]
+        result = [[] for _ in range(self._n)]
+        for i in range(self._n):
+            result[leader_buf[i]].append(i)
+        return list(filter(lambda r: r, result))
+
+
+N, Q = map(int, input().split())
+dsu = DSU(N)
+for _ in range(Q):
+    t, u, v = map(int, input().split())
+    if t == 0:
+        dsu.merge(u, v)
+    else:
+        if dsu.same(u, v):
+            print(1)
+        else:
+            print(0)
