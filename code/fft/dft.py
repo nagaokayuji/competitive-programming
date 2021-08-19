188930966938f5504cfee1bbf1b0372bb2e47245
Yuji Nagaoka <nagaokayuji@team-lab.com>
2021-08-19 19:05:16 +0900

    ✨  fft

--- code/fft/dft.py
+++ code/fft/dft.py
@@ -10,16 +10,17 @@ def dft(a, reverse=False):
     for i in range(n):
         for j in range(n):
             b[i] += a[j] * np.exp(complex(0, sign * i * j * 2 * math.pi / n))
-        if reverse:
-            b[i] /= n
     return b
 
 
-lst = [1+1j, 2+1j, 5+5j, 4+0j]
-
-fft = dft(lst)
-rfft = dft(fft, True)
-
-print(lst)
-print(fft)
-print(rfft)
+def fft(a, reverse=False):
+    ret = a.copy()
+    n = len(a)
+    if n == 1:
+        return a
+    b = fft(a[0:n:2], reverse)
+    c = fft(a[1:n:2], reverse)
+    circle = 2 * math.pi * -1 if reverse else 1
+    for i in range(n):
+        ret[i] = b[i % (n//2)] + c[i % (n//2)] * np.exp(complex(0, circle*i/n))
+    return ret
